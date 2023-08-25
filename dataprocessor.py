5f68f1a7b9c9569b3a34198d4f158555a5bc8df0
yulki <yulki.jay@gmail.com>
2023-08-25 16:26:19 +0800

    modify dataloader

--- dataprocessor.py
+++ dataprocessor.py
@@ -11,24 +11,22 @@ def encode(examples):
 
 ds = ds.map(encode, batched=True) # tokenize the dataset
 
-
-
 train_ds = ds['train']
 val_ds = ds['validation']
 test_ds = ds['test']
 
-print(ds)
-label = ds['train'].features['label']
+# print(ds)
+# label = ds['train'].features['label']
 
-print(label)
-print(ds['train'][0])
+# print(label)
+# print(ds['train'][0])
 def get_train_dataloader():
     train_dataloader = torch.utils.data.DataLoader(
         train_ds,
         batch_size=128,
         num_workers=1,
-        # pin_memory=True,
-        # drop_last=True,
+        pin_memory=True,
+        drop_last=True,
     )
     return train_dataloader
 
@@ -37,8 +35,8 @@ def get_val_dataloader():
         val_ds,
         batch_size=128,
         num_workers=1,
-        # pin_memory=True,
-        # drop_last=True,
+        pin_memory=True,
+        drop_last=True,
     )
     return val_dataloader
 
@@ -47,16 +45,18 @@ def get_test_dataloader():
         test_ds,
         batch_size=2,
         num_workers=1,
-        # pin_memory=True,
-        # drop_last=True,
+        pin_memory=True,
+        drop_last=True,
     )
     return test_dataloader
 
 def main():
     train_dataloader = get_test_dataloader()
     # print(train_dataloader[0])
-    _,labels,input_ids= next(iter(train_dataloader))
-    print(input_ids)
+    batch =  next(iter(train_dataloader))
+    inputs_ids = batch['input_ids']
+    labels = batch['label']
+    print(inputs_ids)
     print(labels)
     
 if __name__ == '__main__':
