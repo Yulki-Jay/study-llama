f09d909c725b463cb6a69e903764ac7bc84af1d0
yulki <yulki.jay@gmail.com>
2023-08-25 13:56:37 +0800

    add test

---
+++ yulki.py
@@ -0,0 +1,20 @@
+import time
+import datetime
+
+start_time = time.time()
+start_datetime = datetime.datetime.now()
+
+
+end_datetime = datetime.datetime.now()
+end_time = time.time()
+
+print(f'start time :{start_datetime} \nend time :{end_datetime} \ntotal time :{end_time - start_time}')
+
+
+st = '  sss_  '
+print(st.replace('_', ' ').strip())
+
+
+a = [0]
+a += [1, 2, 3]
+print(a)
