e58743ab82d594b4b1828effc1ca7f3984a8f40d
yulki <yulki.jay@gmail.com>
2023-09-01 19:52:16 +0800

    add some echo

--- slurm_push.sh
+++ slurm_push.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-#SBATCH --job-name=bert-finetune           # 作业名称
-#SBATCH --output=/home/jiangyunqi/study/study-llama/logs/stufy-llama-finetune-peft_v1.txt        # 输出日志的文件名
+#SBATCH --job-name=llama-finetune           # 作业名称
+#SBATCH --output=/home/jiangyunqi/study/LLM/study-llama/logs/stufy-llama-finetune-peft-precision_v1.txt        # 输出日志的文件名
 #SBATCH --mem=0                   # 任务不限制使用内存
 #SBATCH --partition=gpujl          # 队列名称为gpujl
 
 
 #SBATCH --nodes=1             # This needs to match Trainer(num_nodes=...)
-#SBATCH --gres=gpu:1
-#SBATCH --ntasks-per-node=1   # This needs to match Trainer(devices=...)
+#SBATCH --gres=gpu:4
+#SBATCH --ntasks-per-node=4   # This needs to match Trainer(devices=...)
 #SBATCH --mem=0
 
-echo "4张显卡跑代码"
+echo "这是一次跑量化的任务，使用了8bit量化，优化器是由16精度，记录一下显存的使用情况"
 source /home/jiangyunqi/anaconda3/bin/activate study-llama-v2-finetune      # 激活conda环境
 gpustat 
-srun python3 /home/jiangyunqi/study/study-llama/bert-finetune/bert_finetune.py
+srun python3 /home/jiangyunqi/study/LLM/study-llama/llama-finetune/main.py
+
+# srun python3 /home/jiangyunqi/study/LLM/study-llama/bert-finetune/bert_finetune.py
 
 echo "这个工作完成啦"
